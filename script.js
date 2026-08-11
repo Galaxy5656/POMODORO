96ca542559174e110b73acfed157e949528e8895
Galaxy5656 <105578707+Galaxy5656@users.noreply.github.com>
2026-08-11 21:19:07 +1000

    added button to start timer

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const startingMin = 25;
 let time = startingMin * 60; //converts min to sec
-
+let refreshIntervalID = null;
 const countdownEl = document.getElementById("timer1");
-
-setInterval(updateCountdown, 1000);
+const startButton = document.getElementById("start");
 
 function updateCountdown() {
-    const minutes = Math.floor (time / 60);
-    let seconds = time % 60;
+        const minutes = Math.floor (time / 60);
+        let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countdownEl.innerHTML = `${minutes}:${seconds}`; //writes the string
+        time--;
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`; //writes the string
-    time--;
-
-    if (time < 0) //stops the interval when timer hits 0
-        clearInterval(refreshIntervalid);
+        if (time < 0) {//stops the interval when timer hits 0
+            clearInterval(refreshIntervalID);
+            refreshIntervalID = null;
+    }
 }
+
+
+startButton.addEventListener("click", () => {
+
+    if (refreshIntervalID !== null) return;
+
+    updateCountdown();
+    refreshIntervalID = setInterval(updateCountdown, 1000);
+
+
+});
+
